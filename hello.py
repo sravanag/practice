41143c3c2166fb9a5b2276472a7734fcd87b94f8
sravana G <72683499+sravanag@users.noreply.github.com>
2021-08-06 15:50:15 +0530

    Update hello.py

--- hello.py
+++ hello.py
@@ -4,7 +4,7 @@ def create_dataframe(dict1):
     return df
 
 dict1={"sub":["English","hindi","telugu","urdu","odia","chemistry"],
-             "marks":[90,78,67,58,98,76]
+             "marks":[91,78,67,58,98,76]
        }
 df=create_dataframe(dict1)
 print(df[df['marks']>=70])
