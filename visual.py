3a7c9b231d40658d4a9f1af83d67545ca57e40ac
sravana G <72683499+sravanag@users.noreply.github.com>
2021-08-06 14:41:03 +0530

    Update visual.py

--- visual.py
+++ visual.py
@@ -1,7 +1,7 @@
 import pandas as pd
 import numpy as np
 from hello import create_dataframe
-dict1={'Math': [88, 89, 90], 'Physics': [92, 94, 89], 'Chemistry': [90, 87, 93]}
+dict1={'Math': [89, 89, 90], 'Physics': [92, 94, 89], 'Chemistry': [90, 87, 93]}
 df1=create_dataframe(dict1)
 print(df1)
 for col in df1.columns:
